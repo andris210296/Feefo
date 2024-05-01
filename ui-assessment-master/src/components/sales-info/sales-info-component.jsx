8fd5e7fecdf5f8e2f7e8c5b98f2314e0941acccf
Andris Andermann <andris.andermann@terra.com.br>
2024-05-01 14:49:36 -0300

    Exercise 2 - Updates on components in the bottom

--- ui-assessment-master/src/components/sales-info/sales-info-component.jsx
+++ ui-assessment-master/src/components/sales-info/sales-info-component.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInbox, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import { SalesGrid, GridHead, GridHeadLeftSide, GridHeadRightSide, GridBody, SalesTitle, BoldText } from './sales-info';
+import {
+    SalesGrid,
+    GridHead,
+    GridHeadLeftSide,
+    GridHeadRightSide,
+    GridBody,
+    SalesTitle,
+    BoldText,
+    PercentageResult,
+    SecondaryTitle,
+    Grid,
+    GridItem
+} from './sales-info';
+
 export const SalesInfoComponent = ({ data }) => {
     console.log(data);
+
+
+    const percentageSuccessfulUploads = (data.salesOverview.successfulUploads / data.salesOverview.uploads) * 100;
+
+    const percentageSuccessfulLinesSaved = (data.salesOverview.linesSaved / data.salesOverview.linesAttempted) * 100;
 
     return (
         <>
@@ -14,12 +32,12 @@ export const SalesInfoComponent = ({ data }) => {
                         <div style={{ display: 'flex', alignItems: 'center', color: 'gray' }}>
                             <FontAwesomeIcon icon={faInbox} color="#0AC9F0" />
                             <SalesTitle>Sales</SalesTitle>
+
+                            <div style={{ marginLeft: "700%" }}>
+                                <FontAwesomeIcon icon={faInfoCircle} color="gray" />
+                            </div>
                         </div>
                     </GridHeadLeftSide>
-
-                    <GridHeadRightSide>
-                        <FontAwesomeIcon icon={faInfoCircle} color="gray" />
-                    </GridHeadRightSide>
                 </GridHead>
                 <GridBody>
                     You had <BoldText> {data.salesOverview.uploads} uploads </BoldText> and <BoldText>{data.salesOverview.linesSaved}</BoldText> lines added.
@@ -28,6 +46,24 @@ export const SalesInfoComponent = ({ data }) => {
 
 
             </SalesGrid >
+
+            <Grid>
+                <GridItem>
+                    <PercentageResult>
+                        {(percentageSuccessfulUploads.toFixed(0))}%
+                    </PercentageResult>
+                    <SecondaryTitle>upload success</SecondaryTitle>
+
+                </GridItem>
+                <GridItem>
+                    <PercentageResult>
+                        {(percentageSuccessfulLinesSaved.toFixed(0))}%
+                    </PercentageResult>
+                    <SecondaryTitle>lines saved</SecondaryTitle>
+
+                </GridItem>
+            </Grid>
+
 
         </>
     )
--- ui-assessment-master/src/components/sales-info/sales-info.js
+++ ui-assessment-master/src/components/sales-info/sales-info.js
@@ -17,17 +17,19 @@ export const GridHead = styled.div`
   margin-left: 2%;
 `;
 
-export const GridHeadLeftSide = styled.div`
-  flex: 1;
+export const GridHeadLeftSide = styled.div`  
+  display: flex;
+  justify-content: flex-start;
 `;
 
 export const GridHeadRightSide = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
   flex: 1;
   margin-right: 2%;
   align-items: center;
   color: #a9a9a9;
+  grid-column: 2;
 `;
 
 export const SalesTitle = styled.h2`
@@ -50,4 +52,33 @@ export const GridBody = styled.div`
 export const BoldText = styled.span`
   font-weight: bold;
   margin: 0 5px;
+`;
+
+export const PercentageResult = styled.div`
+  display: flex;
+  color: #3DD655;
+  font-size: 2em;
+  font-weight: bold;
+`;
+
+export const SecondaryTitle = styled.div`
+  display: flex;
+  margin-top: 5px;
+  color: #a9a9a9;
+  font-size: 1.0em;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 3px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const GridItem = styled.div`
+  padding: 10px;
+  background-color: white;
 `;
